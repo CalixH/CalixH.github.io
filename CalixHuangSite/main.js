da8cc97127a55f89742d5992be177cd1297ac7fd
CalixH <c295huan@uwaterloo.ca>
2023-01-16 11:40:04 -0500

    e

--- CalixHuangSite/main.js
+++ CalixHuangSite/main.js
@@ -1,78 +1,98 @@
-import './style.css'
-
+import './style.css';
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// Setup
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'),});
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(25);
+camera.position.setZ(30);
 camera.position.setX(-3);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100, 4.8);
-const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
+// Torus
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
+
 scene.add(torus);
 
+// Lights
+
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5,5,5);
+pointLight.position.set(5, 5, 5);
+
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// Helpers
 
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
 
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
 
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
 
-  star.position.set(x,y,z);
+  star.position.set(x, y, z);
   scene.add(star);
 }
 
 Array(200).fill().forEach(addStar);
 
+// Background
+
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
-// me
+// Avatar
 
-const caxTexture = new THREE.TextureLoader().load('CalixHuang.JPG');
+const caxTexture = new THREE.TextureLoader().load('calixHuang.JPG');
 
-const cax = new THREE.Mesh(
-  new THREE.BoxGeometry(3,3,3),
-  new THREE.MeshBasicMaterial({map: caxTexture})
-);
+const cax = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: caxTexture }));
 
 scene.add(cax);
 
 // orange
 
 const orangeTexture = new THREE.TextureLoader().load('orange.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 
 const orange = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({ map: orangeTexture})
+  new THREE.MeshStandardMaterial({
+    map: orangeTexture,
+    normalMap: normalTexture,
+  })
 );
 
 scene.add(orange);
 
 orange.position.z = 30;
-orange.position.x = -10;
+orange.position.setX(-10);
 
 cax.position.z = -5;
 cax.position.x = 2;
+
+// Scroll Animation
 
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
@@ -84,12 +104,14 @@ function moveCamera() {
   cax.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
-  camera.position.y = t * -0.0002;
   camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
 }
 
 document.body.onscroll = moveCamera;
 moveCamera();
+
+// Animation Loop
 
 function animate() {
   requestAnimationFrame(animate);
@@ -98,7 +120,9 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  controls.update();
+  orange.rotation.x += 0.005;
+
+  // controls.update();
 
   renderer.render(scene, camera);
 }
