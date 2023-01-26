4ebeab74d7e8bbd2164854617d3918eb4446ad95
CalixH <c295huan@uwaterloo.ca>
2023-01-26 11:12:13 -0500

    Update main.js

--- CalixHuangSite/main.js
+++ CalixHuangSite/main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three';
 import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls';
 import { FontLoader } from '../node_modules/three/examples/jsm/loaders/FontLoader';
 import {TTFLoader} from '../node_modules/three/examples/jsm/loaders/TTFLoader';
