59164c0bee1ea1baee873868f83037f4df10ce8a
CalixH <c295huan@uwaterloo.ca>
2023-01-26 01:25:04 -0500

    r

--- CalixHuangSite/main.js
+++ CalixHuangSite/main.js
@@ -4,9 +4,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import {TTFLoader} from 'three/examples/jsm/loaders/TTFLoader';
 import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry';
+import { RGBAFormat } from 'three';
 
 
 // Setup
+var t = 0;
 
 const scene = new THREE.Scene();
 
@@ -44,6 +46,13 @@ const plane = new THREE.Mesh(planeG, material);
 plane.rotation.y = -100;
 plane.rotation.z = -100;
 scene.add(plane);
+
+const plane2 = new THREE.Mesh(planeG, material);
+plane2.rotation.y = -110;
+plane2.rotation.z = -100;
+plane2.position.x = 700;
+plane2.position.z = 200;
+scene.add(plane2);
 
 // const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -108,7 +117,7 @@ scene.background = new THREE.Color(0x000000);
 // Avatar
 
 const caxTexture = new THREE.TextureLoader().load('CalixHuang.JPG');
-const cax = new THREE.Mesh(new THREE.PlaneGeometry(5,5), new THREE.MeshBasicMaterial({map: caxTexture}));
+const cax = new THREE.Mesh(new THREE.PlaneGeometry(3.75,5), new THREE.MeshBasicMaterial({map: caxTexture}));
 cax.rotateZ(-0.2);
 cax.rotateY(-0.3);
 cax.position.x = 3;
@@ -116,27 +125,46 @@ cax.position.z = 5;
 cax.position.y = -0.3;
 scene.add(cax);
 
+// Uni
+const uwTexture = new THREE.TextureLoader().load('uw.png');
+const uw = new THREE.Mesh(new THREE.CircleGeometry(5.4), new THREE.MeshBasicMaterial({map: uwTexture, side: THREE.DoubleSide}));
+uw.position.x = 14;
+uw.position.z = 16;
+uw.position.y = -3;
+scene.add(uw);
+
 const geometry2 = new THREE.TorusKnotGeometry( 1.6, 0.6, 100, 16,1 ,3 );
 const torusKnot = new THREE.Mesh( geometry2, material );
 torusKnot.rotateX(-0.7);
 
 scene.add(torusKnot);
 
-// orange
+// video
+let video = document.getElementById("video");
 
-const orangeTexture = new THREE.TextureLoader().load('orange.jpg');
+let videoTexture = new THREE.VideoTexture(video);
+videoTexture.minFilter = THREE.LinearFilter;
+videoTexture.magFilter = THREE.LinearFilter;
+
+var movieMaterial = new THREE.MeshBasicMaterial({
+  map: videoTexture,
+  side : THREE.FrontSide,
+  toneMapped: false,
+});
 
 const orange = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: orangeTexture
-  })
+  new THREE.PlaneGeometry(72,128),
+  movieMaterial
 );
 
-scene.add(orange);
+// orange.position.z = -80;
+// orange.position.x = 400;
+// orange.position.y = 100;
+orange.rotation.y = -2;
+orange.rotation.z = 2.167;
+orange.rotation.x = 3;
 
-orange.position.z = 30;
-orange.position.setX(-10);
+scene.add(orange);
 
 torusKnot.position.z = -5;
 torusKnot.position.x = 2;
@@ -145,16 +173,27 @@ torusKnot.position.x = 2;
 // Scroll Animation
 
 function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-  orange.rotation.x += 0.05;
-  orange.rotation.y += 0.075;
-  orange.rotation.z += 0.05;
+  t = document.body.getBoundingClientRect().top;
+  //<p id="demo"></p>  for html
+  //document.getElementById("demo").innerHTML = t;
+  uw.rotation.x += 0.03;
+  uw.rotation.y += 0.02;
 
+  orange.position.y = -500 - t * 0.1;
+  orange.position.x = 210 + t * 0.02;
+  orange.position.z = 80 + t * 0.02;
 
-  camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
+  camera.position.z = t * -0.01;
   camera.rotation.y = t * 0.0002;
-  camera.rotation.z = t * 0.0002;
+  if (t > -4000) {
+    camera.rotation.z = t * 0.0002;
+  } else if (t <= -4000 && t > -9000){
+    camera.rotation.z = -0.8;
+  } else {
+    camera.rotation.z = -0.8 - (t + 9000) * 0.0002
+  }
+  //document.getElementById("demo").innerHTML = camera.rotation.z;
 }
 
 document.body.onscroll = moveCamera;
@@ -169,10 +208,13 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  orange.rotation.x += 0.005;
+  
+  uw.rotation.x += 0.01;
+  uw.rotation.y += 0.009;
 
   torusKnot.rotation.z += 0.01;
   // controls.update();
+  videoTexture.needsUpdate = true;
 
   renderer.render(scene, camera);
 }
